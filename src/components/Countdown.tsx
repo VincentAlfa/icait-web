84821a1c1c292f1fed04893dc48bcc9a067c7439
muhammad hanif ramadhani <69223521+haniframadhani@users.noreply.github.com>
2024-12-26 18:35:40 +0700

    feat: add countdown component
    Introduced a countdown component to display a timer for events. The component is customizable and integrates seamlessly into the existing layout.

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,32 @@
+'use client';
+
+import { DateTime } from 'luxon';
 import { Card, CardContent } from './ui/card';
+import { useEffect, useState } from 'react';
 
 const Countdown = ({ className }: { className: string }) => {
+  const [currentTime, setCurrentTime] = useState(
+    DateTime.now().setZone('Asia/Jakarta'),
+  );
+  const targetTime: DateTime =
+    DateTime.fromISO('2025-09-10').setZone('Asia/Jakarta');
+
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const diff = targetTime.toUnixInteger() - currentTime.toUnixInteger();
+      const newdate = DateTime.now().setZone('Asia/Jakarta');
+      setCurrentTime(newdate);
+      setDays(Math.floor(diff / 86400));
+      setHours(Math.floor((diff % 86400) / 3600));
+      setMinutes(Math.floor((diff % 3600) / 60));
+      setSeconds(Math.floor(diff % 60));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [currentTime, targetTime]);
   return (
     <Card
       className={
@@ -11,25 +37,25 @@ const Countdown = ({ className }: { className: string }) => {
         <div className='flex justify-center gap-1 text-slate-50 md:gap-4'>
           <div className='flex w-[4.3rem] flex-col text-center md:w-[4.4rem]'>
             <span className='text-3xl font-bold tabular-nums md:text-4.5xl'>
-              76
+              {days < 10 ? '0' + days : days}
             </span>
             <span className='md:font-base font-normal'>Days</span>
           </div>
           <div className='flex w-[4.3rem] flex-col text-center md:w-[4.4rem]'>
             <span className='text-3xl font-bold tabular-nums md:text-4.5xl'>
-              17
+              {hours < 10 ? '0' + hours : hours}
             </span>
             <span className='md:font-base font-normal'>Hours</span>
           </div>
           <div className='flex w-[4.3rem] flex-col text-center md:w-[4.4rem]'>
             <span className='text-3xl font-bold tabular-nums md:text-4.5xl'>
-              39
+              {minutes < 10 ? '0' + minutes : minutes}
             </span>
             <span className='md:font-base font-normal'>Minutes</span>
           </div>
           <div className='flex w-[4.3rem] flex-col text-center md:w-[4.4rem]'>
             <span className='text-3xl font-bold tabular-nums md:text-4.5xl'>
-              05
+              {seconds < 10 ? '0' + seconds : seconds}
             </span>
             <span className='md:font-base font-normal'>Seconds</span>
           </div>
